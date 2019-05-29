290a43369e3875720cbf9efa869b7ebe78f7b0cb
yunyouming <602986973@qq.com>
2019-05-29 16:14:46 +0800

    update db
    update db

--- db/ela_staydid.sql
+++ db/ela_staydid.sql
@@ -1,2 +1,3 @@
 ALTER TABLE ela_staydid add `PhoneNumbers` varchar(2000) default null;
 ALTER table ela_staydid CHANGE PhoneNumbers PhoneNumber varchar(2000);
+ALTER TABLE ela_staydid add `email` varchar(100) default null;
